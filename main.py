8146ced95225efcf16e79d7cfb946e2affbe76b6
zivzolti-lab <zivzolti@gmail.com>
2025-12-31 20:27:02 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-# Download the data from your GitHub repository
 !wget https://raw.githubusercontent.com/yotam-biu/ps9/main/parkinsons.csv -O /content/parkinsons.csv
 !wget https://raw.githubusercontent.com/yotam-biu/python_utils/main/lab_setup_do_not_edit.py -O /content/lab_setup_do_not_edit.py
 import lab_setup_do_not_edit
