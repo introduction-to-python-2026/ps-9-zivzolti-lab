8cced37cbdcc7df27968305c4143e4b2e1695e89
zivzolti-lab <zivzolti@gmail.com>
2025-12-31 20:25:44 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,35 +1,38 @@
+# Download the data from your GitHub repository
 !wget https://raw.githubusercontent.com/yotam-biu/ps9/main/parkinsons.csv -O /content/parkinsons.csv
 !wget https://raw.githubusercontent.com/yotam-biu/python_utils/main/lab_setup_do_not_edit.py -O /content/lab_setup_do_not_edit.py
 import lab_setup_do_not_edit
 
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 import seaborn as sns
+
+df = pd.read_csv('parkinsons.csv')
+df = df.dropna()
+df.head()
+
+x = df[['PPE', 'DFA']]
+y = df['status']
+
+import sklearn
 from sklearn.preprocessing import MinMaxScaler
+
+scaler = MinMaxScaler(feature_range=(0, 1))
+x = scaler.fit_transform(x)
+
+x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size=0.2, random_state=42)
+
 from sklearn.svm import SVC
+model = SVC()
+model.fit(x_train, y_train)
+
 from sklearn.metrics import accuracy_score
-from sklearn.model_selection import train_test_split
 
-parkinson = pd.read_csv("/content/parkinsons.csv")
-parkinson = parkinson.dropna()
-
-x = parkinson[['DFA', 'PPE']]
-y = parkinson['status']
-
-x_train_i, x_test_i, y_train, y_test = train_test_split(x, y, test_size=0.2)
-
-scaler = MinMaxScaler()
-x_train = scaler.fit_transform(x_train_i)
-x_test = scaler.transform(x_test_i)
-
-
-
-svc = SVC()
-svc.fit(x_train, y_train)
-
-y_pred = svc.predict(x_test)
+y_pred = model.predict(x_test)
 accuracy = accuracy_score(y_test, y_pred)
-print(accuracy)
+print(f'Accuracy: {accuracy}')
 
 import joblib
 
-joblib.dump(svc, 'my_model.joblib')
+joblib.dump(model, 'my_model_parkinson.joblib')
