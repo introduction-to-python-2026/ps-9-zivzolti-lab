13c4555d4e00b568046d6c6c363e5dc0de1c3abe
zivzolti-lab <zivzolti@gmail.com>
2025-12-31 20:02:01 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+!wget https://raw.githubusercontent.com/yotam-biu/ps9/main/parkinsons.csv -O /content/parkinsons.csv
+!wget https://raw.githubusercontent.com/yotam-biu/python_utils/main/lab_setup_do_not_edit.py -O /content/lab_setup_do_not_edit.py
+import lab_setup_do_not_edit
+
 import pandas as pd
 import seaborn as sns
 from sklearn.preprocessing import MinMaxScaler
